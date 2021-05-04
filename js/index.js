b83fa050e189e6722c91edbb6b6ba18058d69b2b
Michael-Kochis <rhishisikk@gmail.com>
2021-05-04 10:34:29 -0500

    First four of ten event listeners implemented

--- js/index.js
+++ js/index.js
@@ -3,10 +3,26 @@ function random(max) {
     return Math.floor(Math.random()*max);
 }
 
+function alertBus(event) {
+    alert("Fun Bus!  Woohoo!");
+    event.stopPropagation();
+}
+
+function blackText(event) {
+    event.target.style.color = 'black';
+    event.stopPropagation();
+}
+
 function buttonColorChange(event) {
     const neoColor = 'rgb(' + random(255) + ', ' + random(255) + ', ' + random(255) + ')';
     event.target.style.backgroundColor = neoColor;
     event.target.style.background = neoColor;
+    event.target.style.transition = '2s'
+    event.stopPropagation();
+}
+
+function greenText(event) {
+    event.target.style.color = 'green';
     event.stopPropagation();
 }
 
@@ -19,3 +35,12 @@ for (let i=0; i<buttonList.length; i++) {
 
 let footer = document.querySelector("footer");
 footer.addEventListener("click", buttonColorChange);
+
+let h4 = document.querySelectorAll("h4");
+h4.forEach((item) => {
+    item.addEventListener("mouseover", greenText);
+    item.addEventListener("mouseleave", blackText);
+});
+
+let busImage = document.querySelector("#image-fun-bus");
+busImage.addEventListener("dblclick", alertBus);
