3d2f88d6ad1627e8be97e88cf16d659d80f19cef
Michael-Kochis <rhishisikk@gmail.com>
2021-05-04 09:39:11 -0500

    Button clicks added.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+function random(max) {
+    return Math.floor(Math.random()*max);
+}
+
+function buttonColorChange(event) {
+    const neoColor = 'rgb(' + random(255) + ', ' + random(255) + ', ' + random(255) + ')';
+    event.target.style.backgroundColor = neoColor;
+    event.target.style.background = neoColor;
+    event.stopPropagation();
+}
+
+
+/* Add event listeners */
+let buttonList = document.querySelectorAll(".btn");
+for (let i=0; i<buttonList.length; i++) { 
+    buttonList[i].addEventListener("click", buttonColorChange);
+}
+
+let footer = document.querySelector("footer");
+footer.addEventListener("click", buttonColorChange);
